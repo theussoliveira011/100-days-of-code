fdfdfe5e2fd5240a6d2c44e35ea4c45059080658
Matheus Oliveira <matheusfotografia987@gmail.com>
2022-10-31 15:19:47 -0300

    /#adicionado-nova-funcao

--- examples/training/funcoes.js
+++ examples/training/funcoes.js
@@ -2,28 +2,35 @@ const fs = require("fs");
 
 
 module.exports = {
-  adicionarAluno: (Aluno, catalogo, nomeArquivo) => {
-    catalogo.push(Aluno);
+
+  adicionarPessoa: (Pessoa, catalogo, nomeArquivo) => {
+    catalogo.push(Pessoa);
     fs.writeFileSync(nomeArquivo, JSON.stringify(catalogo, null, 2));
     return 'Aluno adicionado com sucesso!';
   },
-  buscarPessoa: (array, id) => {
-    return array.find(pessoa => pessoa.id == id);
-     // for(let i = 0; i < array.length;i++){
-     //   if(array[i].id == id){
-     //     return array[i];
-     //   }
-     // } 
-     // return false;
+
+  buscarPessoa: (catalogo, id) => {
+    return catalogo.find(pessoa => pessoa.id == id ? pessoa : false);
+    // for(let i = 0; i < array.length;i++){
+    //   if(array[i].id == id){
+    //     return array[i];
+    //   }
+    // } 
+    // return false;
   },
-  calcularMedia: (array, id) => {
-    let sunrise = this.buscarPessoa(array, id);
+  calcularMedia: (catalogo, id) => {
+    let adicionaMedia = catalogo.find(pessoa => pessoa.id == id ? pessoa : false);
     let media = 0;
-    for(let i = 0; i < sunrise.notas.length; i++){
-      media += sunrise.notas[i];
+    for(let i = 0; i < adicionaMedia.notas.length; i++){
+      media += adicionaMedia.notas[i];
     } 
     media /= 4;
-    sunrise.media = media;
-    return sunrise;
-  }
+    adicionaMedia.media = media;
+    return adicionaMedia;
+  },
+  alteraIdade: (catalogo, id, novaIdade) => {
+    let alteraIdade = catalogo.find(pessoa => pessoa.id == id ? pessoa : false);
+    alteraIdade.age = novaIdade;
+  },
+  
 }
--- examples/training/index.js
+++ examples/training/index.js
@@ -1,31 +1,17 @@
 const funcoes = require("./funcoes");
 const Aluno = require("./Aluno");
 const catalogo = require("./database/catalogo.json");
+const { alteraIdade } = require("./funcoes");
 const nomeArquivo = "./database/catalogo.json";
 
-const { adicionarAluno, buscarPessoa, calcularMedia } = funcoes;
+const { adicionarPessoa, buscarPessoa, calcularMedia } = funcoes;
 
-const regua = [];
-// catalogo.push(aluno1, aluno2);
-adicionarAluno(new Aluno(1, "Rian", 32, "female", [9, 9, 9, 9]), catalogo, nomeArquivo);
-adicionarAluno(new Aluno(2, "Rey", 32, "female", [9, 9, 9, 9]), catalogo, nomeArquivo);
-adicionarAluno(new Aluno(3, "Sayori", 32, "female", [9, 9, 9, 9]), catalogo, nomeArquivo);
-adicionarAluno(new Aluno(4, "Cinthia", 32, "female", [9, 9, 9, 9]), catalogo, nomeArquivo);
+/* ADICIONA Aluno*/
+// adicionarAluno(new Aluno(4, "Cinthia", 32, "female", [9, 9, 9, 9]), catalogo, nomeArquivo);
 
-// console.log(catalogo);
+/* BUSCAR Aluno*/
+// buscarPessoa(catalogo, 4)
 
-/*  Obsoleta, nova function acima! */
-// function adicionarIndividuo(id, name, age, gender, notas){
-//     return catalogo.push({
-//         id: id,
-//         name: name,
-//         age: age,
-//         gender: gender,
-//         notas: notas
-//       });
-// }
-// adicionarIndividuo(5, "Jefferson", 22, "no-gender", [8, 8, 9, 5], catalogo);
-// console.log(catalogo); // {name: 'Jefferson', age: 22, gender: 'no-gender'}
-
-// console.log(calcularMedia(catalogo, 5));
-// console.log(catalogo);
+/* CALCULAR Media*/
+alteraIdade(catalogo, 3, 45);
+console.log(catalogo[2]);
