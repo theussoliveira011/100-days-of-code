2fb6951e928adaf0b2456b82b0bd484c14a2c10a
Matheus Oliveira <matheusfotografia987@gmail.com>
2022-11-01 17:58:30 -0300

    /#adicionado-funcao-getRandomNumber

--- projects/collor-fliper/hex.js
+++ projects/collor-fliper/hex.js
@@ -13,10 +13,14 @@ btn.addEventListener('click', () =>{
   // Loop de repetição para gerar seis números aleatórios.
   for(let i = 0; i<6;i++){
     // busca em posições aleatórias do array até i ser maior que 6
-    hexColor += hex[parseInt(Math.random() * hex.length)];
+    hexColor += hex[getRandomNumber()];
   };
   // atribui ao span o valor de hexcolor
   color.innerText = hexColor;
   // editada o background do body ao valor da hexColor
   document.body.style.backgroundColor = hexColor;
 });
+
+function getRandomNumber(){
+  return Math.floor(Math.random() * hex.length);
+}
