9ccf05c7fbcdbe670e6a251f3137dee975753280
Matheus Oliveira <matheusfotografia987@gmail.com>
2022-11-01 17:37:56 -0300

    /#30-projects-color-fliper

---
+++ projects/collor-fliper/hex.js
@@ -0,0 +1,22 @@
+const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 'A', 'B', 'C', 'D', 'E', 'F'];
+// #000000
+// Seleciona elemento botao
+const btn = document.getElementById('btn');
+
+// Seleciona SPAN do HTML
+const color = document.querySelector('.color');
+
+// Adiciona evento de click ao botao
+btn.addEventListener('click', () =>{
+  // Inicio da cor hex
+  let hexColor = '#';
+  // Loop de repetição para gerar seis números aleatórios.
+  for(let i = 0; i<6;i++){
+    // busca em posições aleatórias do array até i ser maior que 6
+    hexColor += hex[parseInt(Math.random() * hex.length)];
+  };
+  // atribui ao span o valor de hexcolor
+  color.innerText = hexColor;
+  // editada o background do body ao valor da hexColor
+  document.body.style.backgroundColor = hexColor;
+});
